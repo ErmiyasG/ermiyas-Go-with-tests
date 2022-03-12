ad317e4e4b9a3a4663c35a7ed42416ff8d205f3e
Ermiyas Gezahegn <ermiyas10080@gmail.com>
2022-03-12 10:33:03 +0300

    switch added, Hello world and the  modified to respond differently for defferent language

--- 01_hello_world/hello.go
+++ 01_hello_world/hello.go
@@ -1,16 +1,39 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
+const amharic = "Amharic"
 const englishHelloPrifix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const amharicHelloPrefix = "ሰላም "
 
-func Hello(name string) string{
+func Hello(name string, language string) string{
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrifix + name
+	
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case amharic:
+		prefix = amharicHelloPrefix
+	default:
+		prefix = englishHelloPrifix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("", "Spanish"))
 }
