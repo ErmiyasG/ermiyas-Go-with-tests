bdb79f75344e447210252ec20425dbc1c7b7f373
Ermiyas Gezahegn <ermiyas10080@gmail.com>
2022-03-14 08:58:44 +0300

    arrays replaced by slices

--- 04_arrays_and_slices/sum.go
+++ 04_arrays_and_slices/sum.go
@@ -1,8 +1,8 @@
 package arrays_and_slices
 
-import "fmt"
+//import "fmt"
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
@@ -10,7 +10,7 @@ func Sum(numbers [5]int) int {
 	return sum
 }
 
-func main() {
-	nums := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(Sum(nums))
-}
+// func main() {
+// 	nums := []int{1, 2, 3, 4, 5}
+// 	fmt.Println(Sum(nums))
+// }
