71a0c3b3f9aeb064403daf008624cab269453f6a
Ermiyas Gezahegn <ermiyas10080@gmail.com>
2022-03-22 14:10:31 +0300

    Dictionary modified not to overwrite when the values added already exists

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -6,7 +6,16 @@ import (
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not found the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -16,6 +25,16 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
