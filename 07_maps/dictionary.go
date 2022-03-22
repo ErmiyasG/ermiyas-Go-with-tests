f2fbed830f53ec0e2e928c2c216b3903579d7847
Ermiyas Gezahegn <ermiyas10080@gmail.com>
2022-03-22 14:20:23 +0300

    Doctionary is modified to update the definition of existing words

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -9,6 +9,7 @@ type Dictionary map[string]string
 var (
 	ErrNotFound   = errors.New("could not find the word you were looking for")
 	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -36,5 +37,20 @@ func (d Dictionary) Add(word, definition string) error {
 	default:
 		return err
 	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
 	return nil
 }
