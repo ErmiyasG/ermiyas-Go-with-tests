7c703b24355ff813a5ce2a811c73e3d133d68c84
Ermiyas Gezahegn <ermiyas10080@gmail.com>
2022-03-24 10:49:00 +0300

    context completed

--- 14_context/context.go
+++ 14_context/context.go
@@ -1,16 +1,22 @@
 package context
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+		}
+
+		fmt.Fprint(w, data)
 	}
 }
